20d9cf17204e31c98f68d19b7ef23d8f3c2686e3
mattjonesorg <mjones@harrisonstreet.com>
2014-10-01 14:48:52 -0500

    Updated the package name

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 mkdir in
-mkdir out
+mkdir ../hssw-dart-apis
 
 curl -s -o in/auth.json http://localhost:8080/_ah/api/discovery/v1/apis/auth/v1/rest
 
-bin/generate.dart generate --input-dir in --output-dir out --package-name "hssw-apis"
+bin/generate.dart generate --input-dir in --output-dir ../hssw-dart-apis --package-name "hssw-dart-apis"
 
 
