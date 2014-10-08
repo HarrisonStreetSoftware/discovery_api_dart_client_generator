6f07c426713947253a597d18462054b4987ede7b
mattjonesorg <mjones@harrisonstreet.com>
2014-10-08 00:59:20 -0500

    updated the uri for the auth api.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 mkdir in
 mkdir ../hssw_dart_apis
 
-curl -s -o in/auth.json http://localhost:8080/_ah/api/discovery/v1/apis/auth/v1/rest
+curl -s -o in/auth.json http://localhost:8080/_ah/api/discovery/v1/apis/authentication/v1/rest
 
 bin/generate.dart generate --input-dir in --output-dir ../hssw_dart_apis --package-name "hssw_dart_apis"
 
